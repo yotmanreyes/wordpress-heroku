2438d2dbab690b90df3cb7a20d7bfc288f311cd7
Yotman Reyes <32025446+yotman99@users.noreply.github.com>
2020-09-11 16:46:46 -0400

    compile base css in bqrker them

--- web/app/themes/bqrker/functions.php
+++ web/app/themes/bqrker/functions.php
@@ -172,6 +172,12 @@ require get_template_directory() . '/inc/template-functions.php';
 require get_template_directory() . '/inc/customizer.php';
 
 /**
+ * Implement custom Post Types 
+ */
+
+require get_template_directory() . '/inc/custom-entries/card.php';
+
+/**
  * Load Jetpack compatibility file.
  */
 if ( defined( 'JETPACK__VERSION' ) ) {
