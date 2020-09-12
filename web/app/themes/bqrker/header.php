c48ab723eec13ec845dc402381b6d809124271ed
Yotman Reyes <32025446+yotman99@users.noreply.github.com>
2020-09-11 21:05:32 -0400

    add base and heading fonts to theme

--- web/app/themes/bqrker/header.php
+++ web/app/themes/bqrker/header.php
@@ -16,7 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-
+	<link href="https://fonts.googleapis.com/css2?family=Mukta:wght@500;600&family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
