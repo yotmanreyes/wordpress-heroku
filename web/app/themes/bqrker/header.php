f2cbd20e4f4d8ab634e0c407c9b6e414be74c5c7
Yotman Reyes <32025446+yotman99@users.noreply.github.com>
2020-09-11 22:54:42 -0400

    remove branding information, title and tagline of main header

--- web/app/themes/bqrker/header.php
+++ web/app/themes/bqrker/header.php
@@ -28,21 +28,8 @@
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$bqrker_description = get_bloginfo( 'description', 'display' );
-			if ( $bqrker_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $bqrker_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
+				the_custom_logo();
+			?>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
